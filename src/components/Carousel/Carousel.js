dfbbf8d114fe6b4a6a201c23ca64c5bdffa5dff6
Maya Chisem <chisem.maya@gmail.com>
2020-12-25 14:41:59 -0800

    updating carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,175 +1,264 @@
 import React from "react";
-import { MDBCard, MDBCardImage, MDBCardBody, MDBCarousel, MDBCarouselInner, 
-    MDBCarouselItem, MDBContainer,MDBView, MDBCardTitle, MDBCardGroup, MDBCardText } from "mdbreact";
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
+"mdbreact";
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-import "./Carousel.css"
+import "./Carousel.css";
+
 
 const CarouselPage = () => {
 
+
   return (
-    <div id="carousel-cont">
-    <MDBContainer>
+    <MDBContainer style={{height:"100vh"}}>
       <MDBCarousel
         activeItem={1}
-        length={3}
+        length={9}
         showControls={true}
-        showIndicators={false}
+        showIndicators={true}
         interval={false}
         className="z-depth-1"
+        slide
+        id="outer-car"
       >
-        <MDBCarouselInner>
+        <MDBCarouselInner id="inner-car">
           <MDBCarouselItem itemId="1">
-            <MDBView id="card-view">
-
-            <MDBCardGroup>
-                <MDBCard>
-                    <a href="https://secret-beyond-86832.herokuapp.com/home">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/sneaker.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/sneaker.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Sneaker Freakers</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>
-                    </a>
-                    </MDBCard>
-
-                    <MDBCard>
-                    <a href="https://shielded-wave-79654.herokuapp.com/signup.html">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/wtw.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/wtw.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Where's the Weed?</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>  
-                    </a>
-                    </MDBCard>
-
-                    <MDBCard>
-                    <a href="https://acarcentes.github.io/bestappintheworld/">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/ff.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage:"url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/ff.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover"}}                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Food Finder</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>
-                    </a>
-                    </MDBCard>
-                </MDBCardGroup>
-              
+          < MDBView>
+              <img
+                className="image"
+                src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/sneaker.png"
+                alt="Second slide"
+              />
             </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Sneaker Freakers
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://secret-beyond-86832.herokuapp.com/home">
+                        View Project
+                    </a>
+                </Button>
+              </MDBView>
           </MDBCarouselItem>
 
           <MDBCarouselItem itemId="2">
             <MDBView>
-            <MDBCardGroup>
-                
-                <MDBCard>
-                    <a href="http://5stardevelopmentgroup.com/">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/5stardevelopment.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/5stardevelopment.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">5 Star Development</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>  
+              <img
+                className="image"
+                src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/ff.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Food Finder
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Find Finder is a collaborative Javascript based 
+                    application, that allows users to 
+                    search for restaurants in their local area.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://acarcentes.github.io/bestappintheworld/">
+                        View Project
                     </a>
-                </MDBCard>
-
-                <MDBCard>
-                    <a href="https://mchisem.github.io/Planning-in-Pink-HMW-Five/">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/day-planner.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/day-planner.png)", backgroundPosition:"top", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}                       
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Day Planner</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>
-                    </a>
-                    </MDBCard>
-
-                    <MDBCard>
-                    <a href="https://mchisem.github.io/WeatherDashboard/">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/weather-dashboard%20(2).png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/weather-dashboard%20(2).png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Weather Dashboard</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>
-                    </a>
-                    </MDBCard>
-                </MDBCardGroup>
+                </Button>
             </MDBView>
           </MDBCarouselItem>
+
           <MDBCarouselItem itemId="3">
             <MDBView>
-
-                
-            <MDBCardGroup>
-                <MDBCard>
-                    <a href="https://mchisem.github.io/JavascriptTest/develop/index.html">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/javascript-quiz-screenshot.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/javascript-quiz-screenshot.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Javascript Quiz</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>
+              <img
+                className="image"
+                src="https://mchisem.github.io/NewPortfolio/public/assets/images/web/wtw.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Where's the Weed?
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://shielded-wave-79654.herokuapp.com/signup.html">
+                        View Project
                     </a>
-                    </MDBCard>
-
-                    <MDBCard>
-                    <a href="https://mchisem.github.io/password-maker/develop/index.html">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/password-generator.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/password-generator.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}                        
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Password Generator</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>
-                    </a>
-                    </MDBCard>
-
-                    {/* <MDBCard>
-                    <a href="">
-                        <MDBCardImage 
-                        src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/ff.png"
-                        alt="MDBCard image cap" 
-                        style={{ height:"40vh", backgroundImage: "url(https://mayachisem-portfolio.herokuapp.com/assets/images/web/ff.png)", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-                        overlay="white-slight" />
-                        <MDBCardBody>
-                        <MDBCardTitle tag="h5">Sneaker Freakers</MDBCardTitle>
-                            <MDBCardText></MDBCardText>
-                        </MDBCardBody>  
-                    </a>
-                    </MDBCard> */}
-
-                </MDBCardGroup>
-
+                </Button>
             </MDBView>
           </MDBCarouselItem>
+
+          <MDBCarouselItem itemId="4">
+            <MDBView>
+              <img
+                className="image"
+                src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/5stardevelopment.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    5 Star Development
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="http://5stardevelopmentgroup.com/">
+                        View Project
+                    </a>
+                </Button>
+            </MDBView>
+          </MDBCarouselItem>
+
+          <MDBCarouselItem itemId="5">
+          <MDBView>
+              <img
+                className="image"
+                src="https://mchisem.github.io/NewPortfolio/public/assets/images/web/empl-dir.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Employee Directory
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://emp-direc.herokuapp.com/">
+                        View Project
+                    </a>
+                </Button>
+            </MDBView>
+          </MDBCarouselItem>
+
+          <MDBCarouselItem itemId="6">
+          <MDBView>
+              <img
+                className="image"
+                src="https://mayachisem-portfolio.herokuapp.com/assets/images/web/password-generator.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Password Generator
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://mchisem.github.io/password-maker/develop/index.html">
+                        View Project
+                    </a>
+                </Button>
+            </MDBView>
+          </MDBCarouselItem>
+
+          <MDBCarouselItem itemId="7">
+          <MDBView>
+              <img
+                className="image"
+                src="https://mchisem.github.io/NewPortfolio/public/assets/images/web/javascript-quiz-screenshot.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Javascript Quiz
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://mchisem.github.io/JavascriptTest/develop/index.html">
+                        View Project
+                    </a>
+                </Button>
+            </MDBView>
+          </MDBCarouselItem>
+
+          <MDBCarouselItem itemId="8">
+          <MDBView>
+              <img
+                className="image"
+                src="https://mchisem.github.io/NewPortfolio/public/assets/images/web/day-planner.png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Daily Planner
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://mchisem.github.io/Planning-in-Pink-HMW-Five/">
+                        View Project
+                    </a>
+                </Button>
+            </MDBView>
+          </MDBCarouselItem>
+
+          <MDBCarouselItem itemId="9">
+          <MDBView>
+              <img
+                className="image"
+                src="https://mchisem.github.io/NewPortfolio/public/assets/images/web/weather-dashboard%20(2).png"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBView className="content">
+                <Typography component="h5" variant="h5">
+                    Weather Dashboard
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    Sneaker Freakers is a React Library based 
+                    interactive application, that allows users to 
+                    search for sneakers by brand, through a third 
+                    party API.
+                </Typography>
+                <Button className="project-btn">
+                    <a href="https://mchisem.github.io/WeatherDashboard/">
+                        View Project
+                    </a>
+                </Button>
+            </MDBView>
+          </MDBCarouselItem>
+
         </MDBCarouselInner>
       </MDBCarousel>
     </MDBContainer>
-    </div>
   );
 }
 
